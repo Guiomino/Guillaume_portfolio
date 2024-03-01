0895684533f5db4c7ee7632779cd6ba0e456be95
GuillaumeSimplon <estrade.guillaume.simplon@gmail.com>
2024-03-01 17:38:27 +0100

    style(Footer)

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,16 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Créer le footer
   const footer = document.createElement("footer");
 
-  // Div Top
-  const topDiv = document.createElement("div");
-  topDiv.className = "footer-top";
+  // SCROLL TO TOP BUTTON
+  const scrollToTopBtn = document.createElement("img");
+  scrollToTopBtn.className = "scrollToTopBtn";
+  scrollToTopBtn.src = "icons/Footer_ArrowTop.svg";
+  scrollToTopBtn.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+  });
+  footer.appendChild(scrollToTopBtn);
 
-  // Liens
+  // TOP CONTAINER
+  const topDiv = document.createElement("div");
+  topDiv.className = "footerTop";
+
+  // LINKS
   const linksDiv = document.createElement("div");
-  linksDiv.className = "footer-links";
+  linksDiv.className = "footerLinks";
   const linksHeading = document.createElement("h3");
-  linksHeading.textContent = "Links:";
+  linksHeading.textContent = "Links";
   const linksList = document.createElement("ul");
   ["Skills", "Services", "Contact"].forEach((linkText) => {
     const linkItem = document.createElement("li");
@@ -24,90 +33,114 @@ document.addEventListener("DOMContentLoaded", () => {
   linksDiv.appendChild(linksList);
   topDiv.appendChild(linksDiv);
 
-  // Div pour créer ligne séparatrice
+  // SEPARATE LINE
   const separatorDiv1 = document.createElement("div");
-  separatorDiv1.className = "footer-separator";
+  separatorDiv1.className = "footerSeparator";
   topDiv.appendChild(separatorDiv1);
 
-  // Logo
+  // LOGO AND DATE TIME
+  const logoAndDate = document.createElement("div");
+  logoAndDate.className = "logoAndDate";
+
   const logo = document.createElement("img");
   logo.src = "icons/Logo.svg";
   logo.alt = "Logo";
-  topDiv.appendChild(logo);
+  logoAndDate.appendChild(logo);
 
-  // Date sous le logo
   const dateParagraph = document.createElement("p");
   dateParagraph.id = "date";
-  topDiv.appendChild(dateParagraph);
+  logoAndDate.appendChild(dateParagraph);
+  topDiv.appendChild(logoAndDate);
 
-  // Div pour créer ligne séparatrice
+  // SEPARATE LINE 2
   const separatorDiv2 = document.createElement("div");
-  separatorDiv2.className = "footer-separator";
+  separatorDiv2.className = "footerSeparator";
   topDiv.appendChild(separatorDiv2);
 
-  // Liens Sociaux
+  // SOCIAL MEDIA
   const socialMediaDiv = document.createElement("div");
-  socialMediaDiv.className = "footer-social-media";
-  const socialMediaHeading = document.createElement("h3");
-  socialMediaHeading.textContent = "Social Media:";
+  socialMediaDiv.className = "footerSocialMedia";
+  const socialMediaTitle = document.createElement("h3");
+  socialMediaTitle.textContent = "Social Media";
   const socialMediaList = document.createElement("ul");
-  ["Linkedin", "Github", "Instagram", "Site perso"].forEach((socialText) => {
+  const socialMediaData = [
+    {
+      text: "Linkedin",
+      url: "https://www.linkedin.com/in/guillaume-estrade",
+      icon: "Social_Linkedin.svg",
+    },
+    {
+      text: "Github",
+      url: "https://github.com/GuillaumeSimplon",
+      icon: "Skills_Github.svg",
+    },
+    {
+      text: "Instagram",
+      url: "https://www.instagram.com/guiomino",
+      icon: "Social_Instagram.svg",
+    },
+    {
+      text: "Novus",
+      url: "https://www.guiomino.com",
+      icon: "Projects_Novus.svg",
+    },
+  ];
+  socialMediaData.forEach((socialItemMedia) => {
     const socialItem = document.createElement("li");
+
+    const socialIcon = document.createElement("img");
+    socialIcon.src = `icons/${socialItemMedia.icon}`;
+    socialIcon.alt = socialMediaData.text;
+    socialItem.appendChild(socialIcon);
+
     const socialLink = document.createElement("a");
-    socialLink.href = "#"; // Remplacez "#" par vos liens sociaux réels
-    socialLink.textContent = socialText;
+    socialLink.href = socialItemMedia.url;
+    socialLink.textContent = socialItemMedia.text;
+
     socialItem.appendChild(socialLink);
     socialMediaList.appendChild(socialItem);
   });
-  socialMediaDiv.appendChild(socialMediaHeading);
+  socialMediaDiv.appendChild(socialMediaTitle);
   socialMediaDiv.appendChild(socialMediaList);
   topDiv.appendChild(socialMediaDiv);
 
-  // Ajouter le Div Top au footer
   footer.appendChild(topDiv);
 
-  // Div Middle
+  // MIDDLE CONTAINER
   const middleDiv = document.createElement("div");
-  middleDiv.className = "footer-middle";
+  middleDiv.className = "footerMiddle";
 
-  // Bouton Copier l'adresse e-mail
+  // EMAIL BUTTON COPY TO CLIPBOARD
+  const clipboardTitle = document.createElement("h4");
+  clipboardTitle.textContent =
+    "If you prefer to contact me directly from your email box, without using the form:";
+
   const emailButton = document.createElement("button");
   emailButton.textContent = "Copy my email address 📒";
   emailButton.addEventListener("click", () => {
     copyToClipboard("estrade.guillaume.simplon@gmail.com");
     updateButtonState(emailButton);
   });
+  middleDiv.appendChild(clipboardTitle);
   middleDiv.appendChild(emailButton);
 
-  // Ajouter le Div Middle au footer
   footer.appendChild(middleDiv);
 
-  // Div Bottom
+  // BOTTOM CONTAINER
   const bottomDiv = document.createElement("div");
-  bottomDiv.className = "footer-bottom";
+  bottomDiv.className = "footerBottom";
 
-  // Bouton Scroll to Top
-  const scrollToTopButton = document.createElement("button");
-  scrollToTopButton.textContent = "Scroll to Top";
-  scrollToTopButton.addEventListener("click", () => {
-    document.documentElement.scrollTop = 0;
-    document.body.scrollTop = 0;
-  });
-  bottomDiv.appendChild(scrollToTopButton);
-
-  // Copyright
+  // COPYRIGHT
   const copyrightParagraph = document.createElement("p");
   copyrightParagraph.textContent =
     "©2024 - guillaumesimplon.github.io/Guillaume_portfolio";
   bottomDiv.appendChild(copyrightParagraph);
 
-  // Ajouter le Div Bottom au footer
   footer.appendChild(bottomDiv);
 
-  // Ajouter le footer au corps du document
   document.body.appendChild(footer);
 
-  // Fonction pour copier dans le presse-papiers
+  // SCROLL TO TOP BUTTON FUNCTION()
   function copyToClipboard(email) {
     const dummyInput = document.createElement("input");
     document.body.appendChild(dummyInput);
@@ -117,15 +150,15 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.removeChild(dummyInput);
   }
 
-  // Fonction pour mettre à jour l'état du bouton
+  // UPDATE CCLIPBOARD COPY BUTTON FUNCTION()
   function updateButtonState(button) {
-    button.textContent = "Email address copied ! ✅";
+    button.textContent = "EMAIL ADDRESS COPIED ! ✅";
     setTimeout(() => {
       button.textContent = "Copy my email address 📒";
-    }, 3000); // 3000 millisecondes (3 secondes)
+    }, 4000);
   }
 
-  // Fonction pour mettre à jour la date
+  // DATETIME FUNCTION()
   function updateDate() {
     const currentDate = new Date();
     const formattedDate = currentDate
@@ -141,8 +174,6 @@ document.addEventListener("DOMContentLoaded", () => {
       .replace(/at/, "-");
     dateParagraph.innerHTML = formattedDate;
   }
-
-  // Mettre à jour la date toutes les secondes
   setInterval(updateDate, 1000);
   updateDate();
 });
